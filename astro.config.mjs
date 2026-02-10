ae67cb946e0e5c7e68fddd603bc7337ecdc1af11
Jonathen Colon <jonathen@joncolon.dev>
2026-02-09 21:39:49 -0500

    Review build log warnings

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
-
-import vercel from '@astrojs/vercel';
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: cloudflare({
+    imageService: 'compile',
+  }),
   integrations: [tailwind()],
 });
